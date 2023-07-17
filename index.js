564b0a21792bf8a10febe1f3b7be8c4ae6aaf747
Daniel Fuentes <danielfuentesri@gmail.com>
2023-07-17 10:53:33 -0500

    primer cambio

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function laCajaDePandora(numero){
-// proximamente escribiremos codigo aqui
+
+    if (numero % 2 === 0) {
+        return numero.toString(2); 
+
+    } else {
+        return numero.toString(16); 
+    }
+}
+
+function danielFuentes () {
+    return {
+        nombre: "Daniel Fuentes",
+        edad: "26",
+        nacionalidad: "Colombia"
+    }
 }
