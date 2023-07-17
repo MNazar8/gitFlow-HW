113e49762f7eed63b2dc914ad27cd8a9113a75d1
MNazar <nazarmakarena@gmail.com>
2023-07-17 12:16:53 -0300

    Funcion makarena

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 function laCajaDePandora(numero){
-    // if (numero % 2 === 0){
-        // let aBinario = []
-        // for (let i = numero; i >= 1; i=i/2) {
-        //   aBinario.push(Math.floor(i)%2)
-        //   console.log(aBinario);
-        // return aBinario.reverse().join('')
-//         return numero.toString(2);
-//         }
-//     } else {
-//         return numero.toString(16)
-//     }
-// }
-if (numero % 2 === 0) {
-    // Si es un número par, lo convertimos a binario
-    return numero.toString(2);
-  } else {
-    // Si es un número impar, lo convertimos a hexadecimal
-    return numero.toString(16);
-  }
+    if (numero % 2 === 0){
+        let aBinario = []
+        for (let i = numero; i >= 1; i=i/2) {
+          aBinario.push(Math.floor(i)%2)
+        return aBinario.reverse().join('')
+        }
+    } else {
+        return numero.toString(16)
+    }
 }
 
-// console.log(numero);
+function makarena(){
+    return {
+        nombre: 'Makarena',
+        edad: '30',
+        nacionalidad: 'Argentina'
+    }
+}
+
+console.log(laCajaDePandora(18));
+console.log(laCajaDePandora(5));
