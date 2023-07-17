9862f53a4eba1e384daeedac0d313eb77bf539e0
171091 <txng@hotmail.com>
2023-07-17 13:49:56 -0600

    funcion de tania

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-function laCajaDePandora(numero){
-// proximamente escribiremos codigo aqui
+function laCajaDePandora(numero) {
+  // proximamente escribiremos codigo aqui
+  if (numero % 2 === 0) {
+    // Si el número es par, lo convertimos a binario
+    return numero.toString(2);
+  } else {
+    // Si el número es impar, lo convertimos a hexadecimal
+    return numero.toString(16);
+  }
+}
+
+function tania() {
+  return {
+    nombre: "Tania",
+    edad: 31,
+    nacionalidad: "mexicana",
+  };
 }
