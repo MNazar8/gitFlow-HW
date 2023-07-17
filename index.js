07f9a06fedbb94d881b313fba14d7026bda1fa4d
kymi03 <kimysabela03@gmail.com>
2023-07-17 09:51:54 -0500

    cambio en el index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function laCajaDePandora(numero){
-// proximamente escribiremos codigo aqui
+function laCajaDePandora(numero) {
+    // proximamente escribiremos codigo aqui
+    if (numero % 2 === 0) {
+        // Número par
+        return numero.toString(2); // Conversión a binario
+    } else {
+        // Número impar
+        return numero.toString(16); // Conversión a hexadecimal
+    }
 }
+// console.log(laCajaDePandora(10)); 
+console.log(laCajaDePandora(15)); 
+
