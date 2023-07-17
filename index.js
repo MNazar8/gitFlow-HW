b33945b0fcc9c552df1458165eada70a485598e1
pickless141 <masiivan23@gmail.com>
2023-07-17 11:48:20 -0400

    primer commit

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 function laCajaDePandora(numero){
 // proximamente escribiremos codigo aqui
+let decimal = parseInt(numero, 16);
+let binario = decimal.toString(2);
+
+return binario;
+
 }
+function obtenerInfo() { 
+    let nombre = "Ivan Masi";
+    let edad = 25;
+    let nacionalidad = "Paraguaya";
+
+    let info = {
+        nombre: nombre,
+        edad: edad,
+        nacionalidad: nacionalidad
+    };
+    return info;
+}
+
+console.log(obtenerInfo())
