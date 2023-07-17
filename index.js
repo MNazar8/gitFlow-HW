2e0ba2e76e0bf9d53a3f7a572a16f487eda4e527
AlejoC137 <dapatinoa@unal.edu.co>
2023-07-17 10:06:33 -0500

    AlejoC137 - Pandora2

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+
+// para que reciba como parámetro un número entero. Si es un número par, debe convertirlo a binario y retornarlo, y si es impar, convertirlo a hexadecimal y retornarlo.
+
+
 function laCajaDePandora(numero){
 // proximamente escribiremos codigo aqui
+  if (numero % 2 === 0) {
+    // Si el número es par, lo convertimos a binario
+    return numero.toString(2);
+  } else {
+    // Si el número es impar, lo convertimos a hexadecimal
+    return numero.toString(16);
+  }
 }
+
+console.log(laCajaDePandora(10));
